3419a097c62312dc56b8ab14f305c0665f413391
Markus Felten <Markus.Felten@gmx.de>
2011-11-21 21:04:57 +0100

    some moder simple attribute tests added

--- tests/simple.js
+++ tests/simple.js
@@ -4,6 +4,22 @@ var vows = require('vows'),
 
 vows.describe('Attribute').addBatch({
     'Attribute Values': {
+        topic:  new schema.attribute("a1","int"),
+
+        'name is present': function (topic) {
+            assert.equal (topic.name, "a1");
+        },
+        'type is present': function (topic) {
+            assert.equal (topic.type, "int");
+        },
+        'no constraint': function (topic) {
+            assert.equal (topic.constraints.length, 0);
+        },
+        'sql statement': function (topic) {
+            assert.equal (topic.create(), "a1 int");
+        }
+    },
+    'Attribute Values With Contraint': {
         topic:  new schema.attribute("a1","int", [new schema.constraint("not null")]),
 
         'name is present': function (topic) {
