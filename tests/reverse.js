3fba6e0bebf22c8f984acaeac2be62b834c5bae8
Markus Felten <Markus.Felten@gmx.de>
2012-09-17 20:54:10 +0200

    ongoin sql parsing work

--- tests/reverse.js
+++ tests/reverse.js
@@ -24,16 +24,60 @@ vows.describe('Schema From File').addBatch({
         topic:  function() { return db = new sqlite3.Database("tests/reverse.db"); },
         'reverse from database' : {
             topic : function(db) { schema.tables_from_db(db,this.callback); },
-            't1 detected' : function(error,tables) {
+            'comment_date' : function(error,tables) {
                 assert.ifError(error);
 				var t = tables[0];
 				assert.equal(t.name,'comment_date');
+
 				assert.equal(t.attributes[0].name,'date');
 				assert.equal(t.attributes[0].type,'datetime');
 				assert.equal(t.attributes[0].constraints[0].name,'NOT NULL');
+
 				assert.equal(t.attributes[1].name,'comment');
 				assert.equal(t.attributes[1].type,'varchar(255)');
 				assert.equal(t.attributes[1].constraints[0].name,'NOT NULL');
+            },
+            'value_date' : function(error,tables) {
+                assert.ifError(error);
+				var t = tables[1];
+				assert.equal(t.name,'value_date');
+
+				assert.equal(t.attributes[0].name,'date');
+				assert.equal(t.attributes[0].type,'datetime');
+				assert.equal(t.attributes[0].constraints[0].name,'NOT NULL');
+			
+				assert.equal(t.attributes[1].name,'type');
+				assert.equal(t.attributes[1].type,'char(2)');
+				assert.equal(t.attributes[1].constraints[0].name,'NOT NULL');
+
+				assert.equal(t.attributes[2].name,'value');
+				assert.equal(t.attributes[2].type,'float');
+				assert.equal(t.attributes[2].constraints[0].name,'NOT NULL');
+            },
+            'value_type' : function(error,tables) {
+                assert.ifError(error);
+				var t = tables[2];
+				assert.equal(t.name,'value_type');
+
+				assert.equal(t.attributes[0].name,'id');
+				assert.equal(t.attributes[0].type,'char(2)');
+				assert.equal(t.attributes[0].constraints[0].name,'NOT NULL');
+			
+				assert.equal(t.attributes[1].name,'name');
+				assert.equal(t.attributes[1].type,'varchar(65)');
+				assert.equal(t.attributes[1].constraints[0].name,'NOT NULL');
+
+				assert.equal(t.attributes[2].name,'unit');
+				assert.equal(t.attributes[2].type,'varchar(65)');
+				assert.equal(t.attributes[2].constraints[0].name,'NOT NULL');
+				
+				assert.equal(t.attributes[3].name,'ord');
+				assert.equal(t.attributes[3].type,'int(11)');
+				assert.equal(t.attributes[3].constraints[0].name,'NOT NULL');
+				
+				assert.equal(t.attributes[4].name,'color');
+				assert.equal(t.attributes[4].type,'varchar(20)');
+				assert.equal(t.attributes[4].constraints[0].name,'DEFAULT NULL');
             }
         }
      }
