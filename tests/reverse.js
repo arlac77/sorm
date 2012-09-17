9f6380e600d86d72e3f35202e386ac1120ad0664
Markus Felten <Markus.Felten@gmx.de>
2012-09-17 11:00:43 +0200

    ongoing work

--- tests/reverse.js
+++ tests/reverse.js
@@ -5,7 +5,7 @@ var vows = require('vows'),
 
 
 vows.describe('Schema From File').addBatch({
-    'Schema Values': {
+    'Simple db': {
         topic:  function() { return db = new sqlite3.Database("tests/test2.db"); },
         'reverse from database' : {
             topic : function(db) { schema.tables_from_db(db,this.callback); },
@@ -19,5 +19,20 @@ vows.describe('Schema From File').addBatch({
 				assert.equal(t.attributes[0].constraints[1].name,'NOT NULL');
             }
         }
-    }
+    },
+    'consumption db': {
+        topic:  function() { return db = new sqlite3.Database("tests/reverse.db"); },
+        'reverse from database' : {
+            topic : function(db) { schema.tables_from_db(db,this.callback); },
+            't1 detected' : function(error,tables) {
+                assert.ifError(error);
+				var t = tables[0];
+				assert.equal(t.name,'t1');
+				assert.equal(t.attributes[0].name,'a1');
+				assert.equal(t.attributes[0].type,'char(16)');
+				assert.equal(t.attributes[0].constraints[0].name,'PRIMARY KEY');
+				assert.equal(t.attributes[0].constraints[1].name,'NOT NULL');
+            }
+        }
+     }
 }).export(module);
