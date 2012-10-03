3c6cd477888ff5b6cb8d7242b87eca08766655a1
Markus Felten <Markus.Felten@gmx.de>
2012-10-03 16:19:41 +0200

    ongoing schema migration work

--- tests/migrate.js
+++ tests/migrate.js
@@ -105,26 +105,24 @@ vows.describe('Migration').addBatch({
         }
     }
 })
-/*
 .addBatch({
     'Schema Migration Alter Table': {
         topic: function() {
-			var schema = schema.Schema("tests/test4.schema");
-			schema.load(this.callback);
+			schema.Schema("tests/test4.schema",this.callback);
         },
-        'schema hash': function (error,schema) {
-            assert.equal(schema.schemaHash,"a966095f8456ee006bb5b2c8fd4c71676dcd57ef");
+        'schema hash': function(error,schema) {
+            assert.equal(schema.schemaHash,"98a639d364578be35ef3435d81e6d0702760f73f");
         },
-        'schema version': function (error,schema) {
-            assert.equal (schema.version, 3);
+        'schema version': function(error,schema) {
+            assert.equal(schema.version, 3);
         },
         'schema table attributes are there': function (error,schema) {
             assert.equal(schema.tables['t1'].name,"t1");
-            assert.equal(schema.tables['t1'].name,"t2");
             assert.equal(schema.tables['t1'].pk().length,1);
             assert.equal(schema.tables['t1'].pk()[0].name,"a1");
             assert.equal(schema.tables['t1'].pk()[0].type,"char(16)");
 
+            assert.equal(schema.tables['t2'].name,"t2");
             assert.equal(schema.tables['t2'].pk().length,2);
             assert.equal(schema.tables['t2'].pk()[0].name,"a1");
             assert.equal(schema.tables['t2'].pk()[1].name,"a2");
@@ -157,4 +155,4 @@ vows.describe('Migration').addBatch({
             }
         }
     }
-})*/ .export(module);
+	}).export(module);
