dbda9639e2daec5288fcc0c3ac15b1639b957ea0
Markus Felten <Markus.Felten@gmx.de>
2017-02-28 15:53:54 +0100

    wip

---
+++ tests/tables_from_db_test.js
@@ -0,0 +1,14 @@
+/* jslint node: true, esnext: true */
+
+import test from 'ava';
+import tablesFromDatabase from '../src/tables_from_database';
+
+test('foo', t => {
+    t.pass();
+});
+
+test('bar', async t => {
+    const bar = Promise.resolve('bar');
+
+    t.is(await bar, 'bar');
+});
