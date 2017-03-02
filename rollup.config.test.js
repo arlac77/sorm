c91783f0da2298384c6499c1e83cc1b0dbb7b5bc
Markus Felten <Markus.Felten@gmx.de>
2017-03-02 10:52:38 +0100

    chore(test): generate test-bundle.js in ./build dir

--- rollup.config.test.js
+++ rollup.config.test.js
@@ -5,15 +5,16 @@ import multiEntry from 'rollup-plugin-multi-entry';
 
 export default {
   entry: 'tests/**/*_test.js',
+  external: ['ava'],
   plugins: [
-	  babel({
-	    babelrc: false,
-	    presets: ['es2015-rollup'],
-	    exclude: 'node_modules/**'
-	  }),
-	  multiEntry()
-	  ],
+    babel({
+      babelrc: false,
+      presets: ['es2015-rollup'],
+      exclude: 'node_modules/**'
+    }),
+    multiEntry()
+  ],
   format: 'cjs',
-  dest: 'tests/test-bundle.js',
+  dest: 'build/test-bundle.js',
   sourceMap: true
 };
