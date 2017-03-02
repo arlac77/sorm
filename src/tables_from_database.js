844f2c33e3921f6d0e7f2b7fdea431141628cf10
Markus Felten <Markus.Felten@gmx.de>
2017-03-02 11:08:50 +0100

    test: tables_from_database

--- src/tables_from_database.js
+++ src/tables_from_database.js
@@ -6,60 +6,65 @@ export function tablesFromDatabase(db) {
   return new Promise((fullfill, reject) => {
     const tables = new Map();
 
-    tables.set('t1',{ name: 't1'});
-
-/*
     db.all("SELECT name,sql FROM sqlite_master WHERE type='table'", (error, rows) => {
       if (error) {
         reject(error);
         return;
       }
 
-      for (const i in rows) {
-        const row = rows[i];
-        const sql = row.sql.split(/\n/).join(' ');
+      rows.forEach(row => {
+        tables.set(row.name, {
+          name: row.name,
+          sql: row.sql
+        });
+      });
 
-        const m = sql.match(/CREATE\s+TABLE\s+((\"([^\"]+)\")|([a-z][a-z0-9_]*))\s*\((.*)/im);
-        if (m) {
-          const attributes = [];
-          const constraints = [];
-          const name = m[3] ? m[3] : m[4];
-          const ps = {
-            input: m[5]
-          };
-
-          do {
-            if (parse_constraints(ps, constraints)) {
-            } else {
-              const m = ps.input.match(
-                /^\s*((\"[^\"]+\")|([a-z][a-z_0-9]*))\s+([a-z][a-z0-9_]*(\([^\)]+\))?)[\s,]+(.*)/i);
-              if (m) {
-                const aname = unquote(m[1]);
-                const type = m[4];
-                ps.input = m[6];
-
-                const cs = [];
-                parse_constraints(ps, cs);
-
-                const m2 = ps.input.match(/^\s*,\s*(.*)/);
-                if (m2) {
-                  ps.input = m2[1];
-                }
-
-                attributes.push(Attribute(aname, type, cs));
-              } else if (ps.input === ')') {
-                break;
-              } else {
-                break;
-              }
-            }
-          }
-          while (ps.input.length > 0);
-
-          tables[name] = Table(name, attributes, constraints);
-        }
-      }
-*/
       fullfill(tables);
     });
+
+    /*
+        db.all("SELECT name,sql FROM sqlite_master WHERE type='table'", (error, rows) => {
+
+          for (const i in rows) {
+            const row = rows[i];
+            const sql = row.sql.split(/\n/).join(' ');
+
+            const m = sql.match(/CREATE\s+TABLE\s+((\"([^\"]+)\")|([a-z][a-z0-9_]*))\s*\((.*)/im);
+            if (m) {
+              const attributes = [];
+              const constraints = [];
+              const name = m[3] ? m[3] : m[4];
+              const ps = {
+                input: m[5]
+              };
+
+              do {
+                if (parse_constraints(ps, constraints)) {
+                } else {
+                  const m = ps.input.match(
+                    /^\s*((\"[^\"]+\")|([a-z][a-z_0-9]*))\s+([a-z][a-z0-9_]*(\([^\)]+\))?)[\s,]+(.*)/i);
+                  if (m) {
+                    const aname = unquote(m[1]);
+                    const type = m[4];
+                    ps.input = m[6];
+
+                    const cs = [];
+                    parse_constraints(ps, cs);
+
+                    const m2 = ps.input.match(/^\s*,\s*(.*)/);
+                    if (m2) {
+                      ps.input = m2[1];
+                    }
+
+                    attributes.push(Attribute(aname, type, cs));
+                  } else if (ps.input === ')') {
+                    break;
+                  } else {
+                    break;
+                  }
+                }
+              }
+              while (ps.input.length > 0);
+    */
+  });
 }
--- tests/tables_from_db_test.js
+++ tests/tables_from_db_test.js
@@ -1,13 +1,21 @@
 /* jslint node: true, esnext: true */
 
+const sqlite3 = require('sqlite3'),
+  path = require('path');
+
 import test from 'ava';
-import { tablesFromDatabase } from '../src/tables_from_database';
+import {
+  tablesFromDatabase
+}
+from '../src/tables_from_database';
 
 test('foo', t => {
-    t.pass();
+  t.pass();
 });
 
 test('tables', async t => {
-    const tables = await tablesFromDatabase();
-    t.is(tables.get('t1').name, 't1' );
+  const db = new sqlite3.Database(path.join(__dirname, '../tests/fixtures/test2.db'));
+
+  const tables = await tablesFromDatabase(db);
+  t.is(tables.get('t1').name, 't1');
 });
